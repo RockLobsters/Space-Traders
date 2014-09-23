489d8ad2b2b10be021de3e8af44e3a8b80684635
Addison Amiri <addisonamiri@gmail.com>
2014-09-22 21:35:44 -0400

    minor changes to police and market

--- SpaceTrader/src/spacetrader/Market.java
+++ SpaceTrader/src/spacetrader/Market.java
@@ -1,13 +1,13 @@
-import java.util.ArrayList;
-import java.util.Random;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-//package spacetrader;
+package spacetrader;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 /**
  *
