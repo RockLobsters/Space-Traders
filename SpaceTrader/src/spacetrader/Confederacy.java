bab31c426de817a45846733b6a85d9d445983693
Kristen Lawrence <Kristen.e.lawrence@gmail.com>
2014-10-29 14:31:17 -0400

    fixed market good quantities-not so outrageous now

--- SpaceTrader/src/spacetrader/Confederacy.java
+++ SpaceTrader/src/spacetrader/Confederacy.java
@@ -87,16 +87,16 @@ public class Confederacy extends PoliticalSystem implements Serializable{
     @Override
     public ArrayList<Good> supply() {
        ArrayList<Good> availableSupply = new ArrayList<>();
-       availableSupply.add(new Good(GoodType.WATER, rand.nextInt(100) * 100));
-       availableSupply.add(new Good(GoodType.FOOD, rand.nextInt(100) * 100));
-       availableSupply.add(new Good(GoodType.FUR, rand.nextInt(100) * 100));
-       availableSupply.add(new Good(GoodType.ORE, rand.nextInt(100) * 100));
-       availableSupply.add(new Good(GoodType.GAMES, rand.nextInt(100) * 100));
-       availableSupply.add(new Good(GoodType.FIREARMS, rand.nextInt(100) * 100));
-       availableSupply.add(new Good(GoodType.MEDICINE, rand.nextInt(100) * 100));
-       availableSupply.add(new Good(GoodType.MACHINES, rand.nextInt(100) * 100));
-       availableSupply.add(new Good(GoodType.NARCOTICS, rand.nextInt(100) * 100));
-       availableSupply.add(new Good(GoodType.ROBOTS, rand.nextInt(100) * 100));
+       availableSupply.add(new Good(GoodType.WATER, rand.nextInt(10) * rand.nextInt(10)));
+       availableSupply.add(new Good(GoodType.FOOD, rand.nextInt(10) * rand.nextInt(10)));
+       availableSupply.add(new Good(GoodType.FUR, rand.nextInt(10) * rand.nextInt(10)));
+       availableSupply.add(new Good(GoodType.ORE, rand.nextInt(10) * rand.nextInt(10)));
+       availableSupply.add(new Good(GoodType.GAMES, rand.nextInt(10) * rand.nextInt(10)));
+       availableSupply.add(new Good(GoodType.FIREARMS, rand.nextInt(10) * rand.nextInt(10)));
+       availableSupply.add(new Good(GoodType.MEDICINE, rand.nextInt(10) * rand.nextInt(10)));
+       availableSupply.add(new Good(GoodType.MACHINES, rand.nextInt(10) * rand.nextInt(10)));
+       availableSupply.add(new Good(GoodType.NARCOTICS, rand.nextInt(10) * rand.nextInt(10)));
+       availableSupply.add(new Good(GoodType.ROBOTS, rand.nextInt(10) * rand.nextInt(10)));
        return availableSupply;
     }
     
