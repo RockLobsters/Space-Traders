7cc7dbe9ea9f4b57cfe106b77585b57eea6363a9
Kristen Lawrence <Kristen.e.lawrence@gmail.com>
2014-11-05 20:58:53 -0500

    made SystemPool enum

---
+++ SpaceTrader/src/spacetrader/SystemPool.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2014 Kristen Lawrence
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package spacetrader;
+
+/**
+ *
+ * @author Kristen Lawrence
+ */
+public enum SystemPool {
+    A("Atlantia", 1, 6),
+    B("Bardic Kingdom", 11, 2),
+    C("Cyber Alliance", 5, 7),
+    D("Disney System", 8, 3),
+    E("Engles Union", 2, 4),
+    F("Filigree Ltd.", 4, 5),
+    G("Golding Barrier Rim", 0, 0),
+    H("Hippee Settlements", 12, 1),
+    I("iGalaxy", 15, 7),
+    J("Judean People's Front", 16, 1),
+    K("Kapital", 1, 3),
+    L("Luder Planets", 3, 3),
+    M("Mrydyn Cymru", 9, 2),
+    N("New Caledonia", 7, 4),
+    O("Otter Oligopoly", 4, 6),
+    P("People's Front of Judea", 16, 1),
+    Q("QI Panel Empire", 7, 5),
+    R("Retrofuturistic Battle Corp", 10, 6),
+    S("Samsong Galaxy", 15, 7),
+    T("Tutt tribes", 0 ,0),
+    U("Utopia", 14, 0),
+    V("Volpone Militia", 10, 3),
+    W("Worlds of Wodehouse", 11, 3),
+    X("Xerxes Democratic Republic", 6, 2),
+    Y("Yelle", 13, 4),
+    Z("Zcousers", 3, 3);
+    String name;
+    int govType;
+    int techLevel;
+    boolean highLevel = false;
+    SystemPool(String name, int govType, int techLevel) {
+        this.name = name;
+        this.govType = govType;
+        this.techLevel = techLevel;
+        if(techLevel >= 4) 
+            highLevel = true;
+    }
+}
--- SpaceTrader/src/spacetrader/Universe.java
+++ SpaceTrader/src/spacetrader/Universe.java
@@ -48,87 +48,48 @@ public class Universe extends ArrayList<SolarSystem> implements Serializable
     {
         //Map<int,SolarSystem> lowTL = new ArrayList<>();
         //Map<SolarSystem> highTL = new ArrayList<>();
-        SolarSystem systemA = new SolarSystem("Atlantia", new CapitalistState(), 6);
-        highTL.add(systemA);
-        SolarSystem systemB = new SolarSystem("Bardic Kingdom", new Monarchy(), 2);
-        lowTL.add(systemB);
-        SolarSystem systemC = new SolarSystem("Cyber Alliance", new Cybernetic(), 7);
-        highTL.add(systemC);
-        SolarSystem systemD = new SolarSystem("Disney System", new FacistState(), 3);
-        lowTL.add(systemD);
-        SolarSystem systemE = new SolarSystem("Engles Union", new CommunistState(), 4);
-        highTL.add(systemE);
-        SolarSystem systemF = new SolarSystem("Filigree Ltd", new Corporate(), 5);
-        highTL.add(systemF);
-        SolarSystem systemG = new SolarSystem("Golding Barrier Rim", new Anarchy(), 0);
-        lowTL.add(systemG);
-        SolarSystem systemH = new SolarSystem("Hipee Settlements", new PacifistState(), 1);
-        lowTL.add(systemH);
-        SolarSystem systemI = new SolarSystem("iGalaxy", new Technocracy(), 7);
-        highTL.add(systemI);
-        SolarSystem systemJ = new SolarSystem("Judean People's Front", new Theocracy(), 1);
-        lowTL.add(systemJ);
-        SolarSystem systemK = new SolarSystem("Kapital", new CapitalistState(), 5);
-        highTL.add(systemK);
-        SolarSystem systemL = new SolarSystem("Luder Planets", new Confederacy(), 3);
-        lowTL.add(systemL);
-        SolarSystem systemM = new SolarSystem("Myrdyn Cymru", new FeudalState(), 2);
-        lowTL.add(systemM);
-        SolarSystem systemN = new SolarSystem("New Caledonia", new Dictatorship(), 4);
-        highTL.add(systemN);
-        SolarSystem systemO = new SolarSystem("Otter Oligopoly", new Corporate(), 6);
-        highTL.add(systemO);
-        SolarSystem systemP = new SolarSystem("People's Front of Judea", new Theocracy(), 1);
-        lowTL.add(systemP);
-        SolarSystem systemQ = new SolarSystem("QI Panel Empire", new Dictatorship(), 5);
-        highTL.add(systemQ);
-        SolarSystem systemR = new SolarSystem("Retrofuturistic Battle Corp", new MilitaryState(), 6);
-        highTL.add(systemR);
-        SolarSystem systemS = new SolarSystem("Samsong Galaxy", new Technocracy(), 7);
-        highTL.add(systemS);
-        SolarSystem systemT = new SolarSystem("Tutu Tribes", new Anarchy(), 0);
-        lowTL.add(systemT);
-        SolarSystem systemU = new SolarSystem("Utopia", new Satori(), 0);
-        lowTL.add(systemU);
-        SolarSystem systemV = new SolarSystem("Volpone-Forze Armate", new MilitaryState(), 4);
-        highTL.add(systemV);
-        SolarSystem systemW = new SolarSystem("Worlds of Wodehosse", new Monarchy(), 3);
-        lowTL.add(systemW);
-        SolarSystem systemX = new SolarSystem("Xerxes Democratic Republic", new Democracy(), 2);
-        lowTL.add(systemX);
-        SolarSystem systemY = new SolarSystem("Yelle", new SocialistState(), 4);
-        highTL.add(systemY);
-        SolarSystem systemZ = new SolarSystem("Zcousers", new Confederacy(), 3);
-        lowTL.add(systemZ);
+        PoliticalSystem[] govs = {new Anarchy(),
+            new CapitalistState(), 
+            new CommunistState(), 
+            new Confederacy(), 
+            new Corporate(), 
+            new Cybernetic(), 
+            new Democracy(), 
+            new Dictatorship(), 
+            new FascistState(), 
+            new FeudalState(), 
+            new MilitaryState(), 
+            new Monarchy(), 
+            new PacifistState(), 
+            new SocialistState(), 
+            new Satori(), 
+            new Technocracy(), 
+            new Theocracy()
+        };
+               
 
         gameUniverse = new SolarSystem[9];
         Random rand = new Random();
         
-        //make array containing all possible planet names
-        String[] allNames = new String[26*5];
-        int index = 0;
-        for (PlanetNames n : PlanetNames.values()) {
-            allNames[index] = n.getName();
-            index++;
-        }
-        //fill game universe array with systems and fill those systems with planets
-        for (int i = 0; (i+1) < 9; i++) {
-            int r = rand.nextInt(highTL.size());
-            SolarSystem systemHigh = highTL.remove(r);
-            SolarSystem systemLow = lowTL.remove(r);
-            gameUniverse[i] = systemHigh;
-            gameUniverse[i+1] = systemLow;
-            for (int j = 0; j < NUMPLANETS[i]; j++) {
-                String name = allNames[r*5+j];
-                Planet p = new Planet(name);
-                p.setSolarSystem(system);
-                system.addPlanet(p);
-            }
-            wholeUniverse[r] = null;
-            this.add(system);
-            r = rand.nextInt(26);
-            system = wholeUniverse[r];
-        }
+        //TODO
+//        //fill game universe array with systems and fill those systems with planets
+//        for (int i = 0; (i+1) < 9; i++) {
+//            int r = rand.nextInt(highTL.size());
+//            SolarSystem systemHigh = highTL.remove(r);
+//            SolarSystem systemLow = lowTL.remove(r);
+//            gameUniverse[i] = systemHigh;
+//            gameUniverse[i+1] = systemLow;
+//            for (int j = 0; j < NUMPLANETS[i]; j++) {
+//                String name = allNames[r*5+j];
+//                Planet p = new Planet(name);
+//                p.setSolarSystem(system);
+//                system.addPlanet(p);
+//            }
+//            wholeUniverse[r] = null;
+//            this.add(system);
+//            r = rand.nextInt(26);
+//            system = wholeUniverse[r];
+//        }
     }
 
     /**
